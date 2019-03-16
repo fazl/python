2422a9881ad8dce06d5bfab8b603422b89fd1017
= <=>
2019-03-16 19:08:04 +0300

    commented

--- hellotkinter/hellotkinter.py
+++ hellotkinter/hellotkinter.py
@@ -1,6 +1,24 @@
-import tkinter as Tk
+# Loosely following tuturial at
+# http://effbot.org/tkinterbook/tkinter-hello-tkinter.htm
+
+# For Python2 need uppercase T i.e. Tkinter 
+# There is great value in consistency :(
+#
+import tkinter as Tk  
+
+
+# root widget like a Swing JFrame
+# basic window with title and standard controls
+# (there must be a way to set title)
 root = Tk.Tk()
-w = Tk.Label(root, text="Hello, world!")
+#root.title="boo" #No error but doesn't work
+
+# Label whose parent is the root window
+# Can either show text or image
+# We use simple text..
+w = Tk.Label(root, text="Hello, Tkinter!")
+
+# Swing has pack() and setVisible()..
 w.pack()
 root.mainloop()
 
