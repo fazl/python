b560c471f9a3326d717381142abf8482c8c9ccfa
= <=>
2019-03-18 00:53:23 +0300

    Howto add confirmation to exit, and howto use messagebox

---
+++ hellotkinter/message-box.py
@@ -0,0 +1,34 @@
+# Loosely following tuturial at
+# http://effbot.org/tkinterbook/tkinter-events-and-bindings.htm
+
+# For Python2 need uppercase T i.e. Tkinter 
+#
+try:
+    import tkinter as Tk  ## python3: tkinter 
+    import tkinter.messagebox as messagebox    # stackoverflow.com/a/38181986
+except ImportError:
+    import Tkinter as Tk  ## python2: tkinter 
+    import tkMessageBox as messagebox
+
+
+# This creates an extra root window ! wtf
+#if messagebox.askokcancel("Quit", "Really quit?"):
+#    print ("Pressed OK")
+#else:
+#    print ("Canceled")
+ 
+
+# basic window with title and standard controls
+# won't accept args width=200, height=200 
+win = Tk.Tk() 
+frame = Tk.Frame( win, width=200, height=200 )
+
+# click event handler
+def confirmExit():
+    if messagebox.askokcancel("Quit", "Really quit?"):
+        win.destroy()
+
+win.protocol("WM_DELETE_WINDOW", confirmExit)
+win.mainloop()
+#win.destroy()
+
