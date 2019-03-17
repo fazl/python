b7256fb713d319ea36f904e4e86e9df50a81f3ed
= <=>
2019-03-17 23:58:30 +0300

    Add right-click handler

--- hellotkinter/mouse-event.py
+++ hellotkinter/mouse-event.py
@@ -9,19 +9,28 @@ except ImportError:
     import Tkinter as Tk  ## python2: tkinter 
 
 # basic window with title and standard controls
-# (there must be a way to set title)
-win = Tk.Tk()
+win = Tk.Tk() # won't accept args width=200, height=200 
 
 
 # click event handler
 def onClick(event):
     print( "Clicked at", event.x, event.y )
 
+def onRightClick(event):
+    print( "Right-clicked at", event.x, event.y )
 
-# Can we do this directly on a window ?
+
+# Can we do this directly on a window ? Ans: NO.
+#
+# <Button-1> = left click  (or <ButtonPress-1> or just <1>)
+# <Button-2> = middle click
+# <Button-3> = right click
+#
+# Can s/Button/ButtonPress/ or s/Button-//
 #
 frame = Tk.Frame( win, width=200, height=200 )
-frame.bind("<Button-1>", onClick)
+frame.bind("<1>", onClick)
+frame.bind("<3>", onRightClick)
 frame.pack()
 
 win.mainloop()
