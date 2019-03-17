97cd8c9f0a672b9c4ecd002b51bdd1cb02afef22
= <=>
2019-03-17 18:22:00 +0300

    add a command file for convenience

---
+++ hellotkinter/run.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+REM Batch command to run a python program.
+REM Change PY_HOME to match your installation..
+@set PY_HOME=C:\Users\Fazl\AppData\Local\Programs\Python\Python37\python.exe
+@set PY_EXE=%PY_HOME%\python.exe
+%PY% %1
