2636aae394809801b3e00716f26e960f683eae3e
= <=>
2019-03-18 11:39:38 +0300

    Toplevel widget as custom dialog, and wait_window() for modal call..

---
+++ hellotkinter/customdialogs.py
@@ -0,0 +1,55 @@
+# Loosely following tuturial at
+# http://effbot.org/tkinterbook/tkinter-dialog-windows.htm
+#
+# I can run this program like this in a command window:
+#
+# C:\Users\Fazl\AppData\Local\Programs\Python\Python37\python.exe customdialogs.py
+#
+
+# Python2 has package Tkinter not tkinter
+try:
+    import tkinter as Tk  ## python3: tkinter 
+except ImportError:
+    import Tkinter as Tk  ## python2: tkinter 
+
+# Try wrapping stuff into a class
+class MyDialog:
+
+    def print(self):     
+        print( "howdie" )
+    
+    def printMsg(self, msg):     
+        print( msg )
+        
+    def __init__(self,ownerWnd):
+        self.printMsg("In MyDialog.ctor")  
+       
+        self.top = Tk.Toplevel(ownerWnd)
+        Tk.Label(self.top, text="Value").pack()
+        
+        self.e = Tk.Entry(self.top)
+        self.e.pack(padx=5)
+        
+        b = Tk.Button(self.top, text="OK", command=self.ok)
+        b.pack(pady=5)
+        self.top.focus_set()
+    
+    def ok(self):
+        print( "OK.. value is:", self.e.get(), "destroying top" )
+        self.top.destroy()
+
+
+def startWorkflow():
+    d = MyDialog(win)
+    win.wait_window(d.top)  #Must know internals of dialog ?
+    
+# script continues here
+win = Tk.Tk()
+
+
+b = Tk.Button(win, text="Start worflow", command=startWorkflow).pack()
+win.update()
+
+win.mainloop()
+
+
