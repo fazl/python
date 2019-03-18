f5bc3ada846b2efd3f8ca77b3db9e9d960137eb4
fazl <5173704+fazl@users.noreply.github.com>
2019-03-18 14:37:47 +0300

    Cope with args<=9, allows running tabnanny etal

--- hellotkinter/run.bat
+++ hellotkinter/run.bat
@@ -3,4 +3,4 @@ REM Batch command to run a python program.
 REM Change PY_HOME to match your installation..
 @set PY_HOME=C:\Users\Fazl\AppData\Local\Programs\Python\Python37\python.exe
 @set PY_EXE=%PY_HOME%\python.exe
-%PY% %1
+%PY% %1 %2 %3 %4 %5 %6 %7 %8 %9
