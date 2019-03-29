f203449bcf7cffa5a05057d600106651be3a9e79
fazl <5173704+fazl@users.noreply.github.com>
2019-03-29 22:21:56 +0300

    Revert to latest py release

--- checktabs.bat
+++ checktabs.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 REM Batch command to check a python program for ambiguous indentation
 REM Change PY_HOME to match your installation..
-@set PY_HOME=C:\Python33
+@set PY_HOME=C:\Users\Fazl\AppData\Local\Programs\Python\Python37-32
 @set PY_EXE=%PY_HOME%\python.exe
 %PY% %PY_HOME%\lib\tabnanny.py -v %1
