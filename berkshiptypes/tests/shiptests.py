e9331f6f7fc3fb4ddf444c724dcde02308ce1426
fazl <5173704+fazl@users.noreply.github.com>
2019-03-31 19:09:54 +0300

    Added a few more tests and tidied up for Berk

--- berkshiptypes/tests/shiptests.py
+++ berkshiptypes/tests/shiptests.py
@@ -1,50 +1,83 @@
 from shiptypes import fewestBricks
 
-#Test script runs here
+# change True to False to reduce logging output
+def trace(msg:str):
+    if False:
+        print(msg)
 
-#for n in range(10):    print(n);
-#for n in range(10,0,-1):    print(n);
-
-
-#fewest = fewestBricks([1,5,6,9],40)
-# fewest = fewestBricks([1],40)
-# assert 40 == fewest, "Expected 40, got %d [from fewestBricks([1],40)]." % fewest
-#
-# fewest = fewestBricks([3],7)
-# assert None == fewest, "Expected None, got %d [from fewestBricks([3],7)]." % fewest
+def getItemCount( counts ) -> int :
+    items = 0
+    for i in counts:
+        items += i
+    return items
 
 def describe(target, sizes, counts) -> str:
     assert len(sizes)==len(counts)
     ret = "Got "
-    sum = 0
+    totLen = 0
     for i in range(len(sizes)):
         ret += "%d*%d" % (counts[i], sizes[i]) + (" + " if i<len(sizes)-1 else "" )
-        sum += counts[i] * sizes[i]
-    ret += ", totals: %d" % sum
-    assert target == sum
+        totLen += counts[i] * sizes[i]
+    ret += ", total len: %d" % totLen
+    ret += ", items: %d" % getItemCount(counts)
     return ret
 
-def flatVersion():
-    sizes = [9,6,5,1]
+#ASSUMES sizes sorted DESCENDING
+#hardcoded for array size 4
+#returns list of pairs with first entry being count array, second being sum of counts
+#list is sorted with best solution with smallest count at start
+def generateConfigs(sizes:[], target:int) -> list:
+    assert 4==len(sizes)
     (s0,s1,s2,s3)=(sizes[0],sizes[1],sizes[2],sizes[3])
-    target = 30
-    print( "target:%d, sizes: %s"%(target, sizes) )
+    assert s3<=s2 and s2<=s1 and s1<=s0
+    configs = []
+    trace( "target:%d, sizes: %s"%(target, sizes) )
     i0Max = int(target/s0)
     for i0 in range(i0Max,-1,-1):
         tgt0 = target-i0*s0
-        print( "i%d=%d leaves %d to fill"%(0, i0, tgt0) )
+        trace( "i%d=%d leaves %d to fill"%(0, i0, tgt0) )
         i1Max = int(tgt0/s1)
         for i1 in range(i1Max,-1,-1):
             tgt1 = tgt0-i1*s1
-            print("\ti%d=%d leaves %d to fill"%(1, i1, tgt1))
+            trace("\ti%d=%d leaves %d to fill"%(1, i1, tgt1))
             i2Max = int(tgt1/s2)
             for i2 in range(i2Max,-1,-1):
                 tgt2 = tgt1-i2*s2
-                print("\t\ti%d=%d leaves %d to fill"%(2, i2, tgt2))
-                # No loop for i3, it either can work or not
-                i3Max = int(tgt2/s3)
-                tgt3 = tgt2-i3Max*s3
-                print( describe(sizes,[i0,i1,i2,i3Max]) )
-                if 0 != tgt3: print("Fails with remainder:", tgt3)
+                trace("\t\ti%d=%d leaves %d to fill"%(2, i2, tgt2))
 
-flatVersion()
+                # No loop for i3, it either can work or not
+                i3 = int(tgt2/s3)
+                tgt3 = tgt2-i3*s3
+                assert 0 <= tgt3
+                counts = [i0, i1, i2, i3]
+                if 0 == tgt3:
+                    trace( "counts is %s"%counts)
+                    trace( describe(target, sizes, counts) )
+                    configs.append((counts, getItemCount(counts)))
+                else:
+                    trace( "Dropping: %s bc: %s (rem: %d)"%
+                           (counts, describe(target,sizes,counts), tgt3) )
+    # configs.sort(lambda x: x[1]) #sort on the second tuple element FAILS
+    return sorted(configs, key=lambda x: x[1])  #smallest itemcount first
+
+
+
+target = 30
+sizes = [11,9,6,1] #must be ascending sorted
+configs = generateConfigs(sizes, target)
+print("Results: ")
+for i in range(len(configs)) : print(configs[i])
+print("Best is:", configs[0])
+print(describe(target, sizes, configs[0][0]))
+
+assert 4 == generateConfigs(sizes, target)[0][1]
+
+(target,sizes) = (40,[9,6,5,1])
+assert 6 == generateConfigs(sizes, target)[0][1]
+
+(target,sizes) = (30,[9,6,5,1])
+assert 4 == generateConfigs(sizes, target)[0][1]
+
+(target,sizes) = (50,[10,7,3,2])
+assert 5 == generateConfigs(sizes, target)[0][1]
+
