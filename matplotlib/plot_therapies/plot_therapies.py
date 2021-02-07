9f9101c9a31fdc21b1c9c530c955c21f47125905
Fazl <5173704+fazl@users.noreply.github.com>
2021-02-07 20:56:44 +0000

    plot_therapies plots line joining supplied data point arrays

--- matplotlib/plot_therapies/plot_therapies.py
+++ matplotlib/plot_therapies/plot_therapies.py
@@ -1,27 +1,37 @@
-import csv
-
-def readCsv(plotCsv):
-    mS = []
-    mU = []
-    with open(plotCsv, 'r') as f:
-        c = csv.reader(f, delimiter = ",")
-        for row in c:
-            #print("row: ", row)
-            #force error if it cant find 3 entries
-            time, ideal, real = row[0], row[1], row[2]
-            mS.append(row[0])
-            mU.append(row[1])        
-    #        if row[0] == "mS":
-    #            mS.append(row[1:])
-    #        elif row[0] == "mU":
-    #            mU.append(row[1:])
-
-    for i in range(len(mS)):
-        print("mS:%s, mU:%s" % (mS[i], mU[i]) )
-    return mS, mU
-    
+# import csv
+# 
+# def readCsv(plotCsv):
+#     xValues = []
+#     yValues = []
+#     with open(plotCsv, 'r') as f:
+#         c = csv.reader(f, delimiter = ",")
+#         for row in c:
+#             #print("row: ", row)
+#             #force error if it cant find 3 entries
+#             time, ideal, real = row[0], row[1], row[2]
+#             xValues.append(row[0])
+#             yValues.append(row[1])        
+#     return xValues, yValues
     
 # Try it:
+# mS, mU = readCsv("plot.csv")
+# print("Got  %s times and %s amounts" % (len(mS), len(mU)))
+# assert len(mS)==len(mU), "Oops different lengths mS: %s vs mU: %s" % (len(mS), len(mU))
 
-readCsv("plot.csv")
+# Artificial setup, just to see if two arrays works
+mS = [0,2,4,6,10]
+mU = [0,1,4,5,9]
 
+import matplotlib.pyplot as plt
+import numpy as np
+#from my_plotter import my_plotter;
+
+fig, ax = plt.subplots(1, 1)
+
+plt.title('Artificial: Therapies Only')
+plt.xlabel('mS')
+plt.ylabel('mU')
+plt.legend()
+
+out = ax.plot(mS, mU)
+plt.show()
